7179e093b98457ae0cba6b94575b82e2bb72b0b9
nunees <felipe_silva_nunes@hotmail.com>
2023-02-07 21:15:29 -0300

    feat: add input text and button, also fix element position on Home screen

--- screens/Home/index.tsx
+++ screens/Home/index.tsx
@@ -1,16 +1,27 @@
-import { Image, TextInput, View } from "react-native";
+import { Image, TextInput, TouchableOpacity, View } from "react-native";
+import Icon from "react-native-vector-icons/AntDesign";
 
-import { styles } from "./styles";
+import { styles, colors } from "./styles";
 
 export default function Home() {
   return (
     <View style={styles.container}>
-      <View style={styles.logo}>
-        <Image source={require("../../assets/Logo.png")} />
+      <View style={styles.header}>
+        <Image
+          style={styles.logo}
+          source={require("../../assets/images/Logo.png")}
+        />
       </View>
 
       <View style={styles.form}>
-        <TextInput style={styles.input} placeholder={"Adicione uma tarefa"} />
+        <TextInput
+          style={styles.input}
+          placeholder={"Adicione uma nova tarefa"}
+          placeholderTextColor={"#808080"}
+        />
+        <TouchableOpacity style={styles.button}>
+          <Icon name="pluscircleo" size={16} color={colors.gray100} />
+        </TouchableOpacity>
       </View>
     </View>
   );
--- screens/Home/styles.ts
+++ screens/Home/styles.ts
@@ -1,17 +1,59 @@
 import { StyleSheet } from "react-native";
 
+export const colors = {
+  danger: "#E25858",
+  gray100: "#F2F2F2",
+  gray200: "#D9D9D9",
+  gray300: "#808080",
+  gray400: "#333333",
+  gray500: "#262626",
+  gray600: "#1A1A1A",
+  gray700: "#0D0D0D",
+  purple: "#8284FA",
+  purpleDark: "#5E60CE",
+  blue: "#4EA8DE",
+  blueDark: "#1E6F9F",
+};
+
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#262626",
+    backgroundColor: colors.gray600,
   },
-  logo: {
-    backgroundColor: "#0D0D0D",
+  header: {
+    backgroundColor: colors.gray700,
     height: 173,
     justifyContent: "center",
     alignItems: "center",
   },
+  logo: {
+    width: 110,
+    height: 32,
+  },
   form: {
     width: "100%",
+    flexDirection: "row",
+    justifyContent: "center",
+    marginVertical: -30,
+  },
+  input: {
+    width: 271,
+    height: 54,
+    marginRight: 4,
+    padding: 16,
+    paddingRight: 63,
+    color: colors.gray100,
+    backgroundColor: colors.gray500,
+    borderRadius: 6,
+    textAlign: "center",
+    fontSize: 16,
+  },
+  button: {
+    width: 54,
+    height: 54,
+    borderRadius: 6,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: colors.blueDark,
   },
 });
